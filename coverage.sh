c8934d149da68a6d02c9180eecaeac1b4035680f
Kai Rasilainen <kai.rasilainen@gmail.com>
2025-09-04 15:44:14 +0300

    Coverage script updated for warnings.

--- coverage.sh
+++ coverage.sh
@@ -14,8 +14,8 @@ for test_case_name in $test_list; do
 
     lcov --directory build --zerocounters > /dev/null
     build/test_number_to_string --gtest_filter="${test_case_name}" || echo "Test ${test_case_name failed}"
-    lcov --capture --directory ${BUILD_DIR} --output-file ${COVERAGE_INFO} --ignore-errors mismatch > /dev/null
-    lcov --ignore-errors unused,unused --ignore-errors empty --remove ${COVERAGE_INFO} '/usr/*' '*/tests/*' --output-file ${COVERAGE_INFO} > /dev/null
-    genhtml ${COVERAGE_INFO} --output-directory ${COVERAGE_DIR} > /dev/null
+    lcov --capture --directory ${BUILD_DIR} --output-file ${COVERAGE_INFO} --ignore-errors mismatch,unsupported,inconsistent > /dev/null
+    lcov --ignore-errors unused,empty,inconsistent --remove ${COVERAGE_INFO} '/usr/*' '*/tests/*' --output-file ${COVERAGE_INFO} > /dev/null
+    genhtml ${COVERAGE_INFO} --output-directory ${COVERAGE_DIR} --ignore-errors missing > /dev/null
     echo "Coverage report generated in ${COVERAGE_DIR}/index.html"
 done
