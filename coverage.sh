67236059ce44d51417f69fa1419b2c1abfea8ccf
Kai Rasilainen <kai.rasilainen@gmail.com>
2025-10-07 11:37:49 +0000

    Coverage fix.

--- coverage.sh
+++ coverage.sh
@@ -48,9 +48,7 @@ lcov --gcov-tool "$GCOV_TOOL" \
 --base-directory "." \
 --no-checksum \
 --rc "lcov_branch_coverage=1" \
---rc "lcov_base_dir=${PWD}" \
---rc "lcov_absolute_path=1" \
---rc "break_linkage=1" \
+--include '*/src/*' \
 --ignore-errors mismatch,empty 2> /dev/null
 
 echo "--- 4. Filtering out system headers ---"
